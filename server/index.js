05e369ec101093eb91d91a7493844a7133056c5c
Elly Lu <y0722cha@gmail.com>
2023-05-26 19:31:38 +0800

    add cors option

--- server/index.js
+++ server/index.js
@@ -9,6 +9,11 @@ const cors = require("cors");
 const authRoute = require("./routes/auth-route");
 const petsRoute = require("./routes/pets-route");
 const port = process.env.PORT || 8000;
+const corsOptions ={
+  origin:'http://localhost:8000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
 
 //connect to MongoDB
 mongoose
@@ -24,7 +29,7 @@ mongoose
 //Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use("/api/user", authRoute);
 app.use(
